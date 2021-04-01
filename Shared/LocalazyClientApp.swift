7b90be4176b5b62aa496b537177ff8faded3427d
Jaroslav Pulik <jaroslav.pulik@gmail.com>
2021-04-01 11:39:12 +0200

    Initial Commit

---
+++ Shared/LocalazyClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalazyClientApp.swift
+//  Shared
+//
+//  Created by Jaroslav Pulik on 01/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct LocalazyClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
