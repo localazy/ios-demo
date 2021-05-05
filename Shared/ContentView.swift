f9c605d155dd02660ec462fe45627dd9e40cedda
Jaroslav Pulik <jaroslav.pulik@gmail.com>
2021-05-05 13:13:09 +0200

    Added missing stuff

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -7,11 +7,71 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+#if os(macOS)
+import Localazy_macOS
+#else
+import Localazy_iOS
+#endif
+
+final class ContentViewModel: ObservableObject {
+    
+    @Published var didLoadLocalizedStrings: Bool = false
+    
+    init() {
+        NotificationCenter.default.publisher(for: .localazyStringsLoaded)
+            .map { _ in true }
+            .assign(to: &$didLoadLocalizedStrings)
     }
+    
+}
+
+struct ContentView: View {
+    
+    @StateObject var contentViewModel = ContentViewModel()
+    
+    var body: some View {
+        VStack {
+            Text("Hello".localazyLocalized)
+                .padding()
+            Button(action: { _ = Localazy.shared.getString(for: "Just-in-source-lang") }) {
+                Text("Access key without translation")
+            }
+        }
+            .onAppear(perform: onAppear)
+            .onChange(of: contentViewModel.didLoadLocalizedStrings) { _ in
+                onStringsLoaded()
+            }
+            .onReceive(NotificationCenter.default.publisher(for: .localazyMissingTextFound)) { notif in
+                let id = notif.userInfo?["id"] as? LocalazyID
+                let locale = notif.userInfo?["locale"] as? LocalazyLocale
+                let key = notif.userInfo?["key"] as? String
+                print(id, locale, key)
+            }
+    }
+    
+    func onAppear() {
+//        Localazy.shared.forceReload()
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(50)) {
+            Localazy.shared.setStatsEnabled(false)
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(100)) {
+            Localazy.shared.setStatsEnabled(true)
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(150)) {
+            Localazy.shared.setStatsEnabled(false)
+        }
+    }
+    
+    func onStringsLoaded() {
+        let locales = Localazy.shared.getLocales()
+        print(locales)
+        
+        let string = Localazy.shared.getString(for: "Hello")
+        print(string)
+        
+        print("Hello".localazyLocalized)
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
